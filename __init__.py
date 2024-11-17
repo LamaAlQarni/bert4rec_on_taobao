6e2e4135424448f3333cbe1171615ce620f8eea3
LAMA <141254648+LamaAlQarni@users.noreply.github.com>
2024-11-18 01:27:51 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-from model.bert4rec import BERT4Rec
-from src.dataset import TaoBaoDataset
-from src.train import train, compute_loss
-from src.utils import set_seed, get_device
-from src.evaluate import calculate_metrics_batch
+from .model.bert4rec import BERT4Rec
+from .src.dataset import TaoBaoDataset
+from .src.train import train, compute_loss
+from .src.utils import set_seed, get_device
+from .src.evaluate import calculate_metrics_batch
