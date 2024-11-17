418eb6edff171dfba28564c9be33f3f86f05265b
LAMA <141254648+LamaAlQarni@users.noreply.github.com>
2024-11-18 01:29:38 +0300

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -9,8 +9,9 @@ import random
 from tqdm import tqdm
 
 class TaoBaoDataset(Dataset):
-    def __init__(self, data_dir, max_len=15, split='train', test_ratio=0.2, random_seed=42, mask_prob=0.15):
+    def __init__(self, data_dir, max_len=15,n_rows=80000000 split='train', test_ratio=0.2, random_seed=42, mask_prob=0.15):
         self.data_dir = data_dir
+        self.n_rows=n_rows
         self.max_len = max_len
         self.split = split
         self.test_ratio = test_ratio
@@ -29,7 +30,7 @@ class TaoBaoDataset(Dataset):
         taobao_file= os.path.join(self.data_dir, 'UserBehavior.csv')
         column_names = ['user_id', 'item_id', 'category_id', 'behavior_type', 'timestamp']
         # 80M was the highest number of row that can be loaded
-        self.df=pd.read_csv(taobao_file,nrows=80000000,header=None,names=column_names)
+        self.df=pd.read_csv(taobao_file,nrows=self.n_rows,header=None,names=column_names)
         self.df=self.df[self.df['behavior_type']=='buy']
         print('Reading Data: Done!')
         self.item_id_to_idx={id: idx + 1 for idx, id in enumerate(self.df['item_id'].unique())}
